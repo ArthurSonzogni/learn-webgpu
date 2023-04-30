fd3dc4d29b0dea272b3be74e874e2bc0696573b3
ArthurSonzogni <sonzogniarthur@gmail.com>
2023-04-30 16:06:59 +0200

    Add GLFW

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main(int, char**) {
-  std::cout << "Hello, world!" << std::endl;
+  if (!glfwInit()) {
+    std::cerr << "Could not initialize GLFW!" << std::endl;
+    return 1;
+  }
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Learn WebGPU", NULL, NULL);
+  while (!glfwWindowShouldClose(window)) {
+    // Check whether the user clicked on the close button (and any other
+    // mouse/key event, which we don't use so far)
+    glfwPollEvents();
+  }
+  // [...] Use the window
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
   return 0;
 }
